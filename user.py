616ed8bdfd962ef92d5a63223d5e4897effb4fe7
gongju-byte <nurulizzatihayat@gmail.com>
2024-12-20 12:22:04 +0800

    corrected some errors

--- user.py
+++ user.py
@@ -7,7 +7,7 @@ user_blueprint = Blueprint('user', __name__, url_prefix='/user')
 def homepage():
     return render_template("/user/homepage.html")
 
-@user_blueprint.route('/user/add-to-cart', methds=['POST'])
+@user_blueprint.route('/user/add-to-cart', methods=['POST'])
 def add_to_cart():
     product_data = request.get_json().get('product')
     product_name = product_data.get('name')
@@ -35,12 +35,12 @@ def add_to_cart():
     session.modified = True
     return jsonify({'success': True, 'cart': session['cart']})
 
-@user_blueprint('/user/get-cart', methods=['GET'])
+@user_blueprint.route('/user/get-cart', methods=['GET'])
 def get_cart():
     cart = session.get('cart', {})
     return jsonify(cart)
 
-@user_blueprint('/user/remove-from-cart', methods=['POST'])
+@user_blueprint.route('/user/remove-from-cart', methods=['POST'])
 def remove_from_cart():
     product_name = request.get_json().get('name')
     if 'cart' in session and product_name in session['cart']:
@@ -48,7 +48,7 @@ def remove_from_cart():
         session.modified = True
     return jsonify({'success': True, 'cart': session.get('cart', {})})
 
-@user_blueprint('/user/cart')
+@user_blueprint.route('/user/cart')
 def cart():
     cart_items = session.get('cart', {})
     total_price = sum(item['price'] * item['quantity'] for item in cart_items.values())
@@ -59,7 +59,7 @@ def cart():
     ]
     return render_template('/homepage/Cart.html', cart_items=cart_list, total_price=total_price)
 
-@user_blueprint('/user/checkout', methods=['GET', 'POST'])
+@user_blueprint.route('/user/checkout', methods=['GET', 'POST'])
 def checkout():
     if request.method == 'POST':
         shipping_address = request.form.get('shipping_address')
@@ -82,4 +82,3 @@ def checkout():
     total_price = sum(item['price'] * item['quantity'] for item in cart_items.values())
     total_price = round(total_price, 2)
     return render_template('/homepage/Checkout.html', cart_items=cart_items, total_price=total_price)
-#routes here
