a1fbe4b71ff034a7ef9b08ddb46aa2a963ff8021
nicktkl <nictankl@gmail.com>
2024-12-21 20:32:23 +0800

    Updated Products' Table

--- db/schema.sql
+++ db/schema.sql
@@ -66,13 +66,17 @@ CREATE TABLE products(
 
 INSERT INTO products(product_id, name, description, price, stock, category_id, image_url, status)
 VALUES
-    (1, 'Testing 01', 'This product is just a testing due to the current situation is testing.', 9.99, 1, 1, 'static/images/dummy.png', 'active'),
-    (2, 'Testing 02', 'This product is just a testing due to the current situation is testing.', 19.99, 2, 2, 'static/images/dummy2.png', 'active'),
-    (3, 'Testing 03', 'This product is just a testing due to the current situation is testing.', 29.99, 3, 3, 'static/images/dummy3.png', 'active'),
-    (4, 'Testing 04', 'This product is just a testing due to the current situation is testing.', 39.99, 4, 4, 'static/images/dummy4.png', 'active'),
-    (5, 'Testing 05', 'This product is just a testing due to the current situation is testing.', 49.99, 5, 5, 'static/images/dummy5.png', 'active'),
-    (6, 'Testing 06', 'This product is just a testing due to the current situation is testing.', 59.99, 6, 6, 'static/images/dummy6.png', 'active'),
-    (7, 'Testing 07', 'This product is just a testing due to the current situation is testing.', 69.99, 7, 7, 'static/images/dummy7.png', 'active'),
-    (8, 'Testing 08', 'This product is just a testing due to the current situation is testing.', 79.99, 8, 8, 'static/images/dummy8.png', 'active'),
-    (9, 'Testing 09', 'This product is just a testing due to the current situation is testing.', 89.99, 9, 9, 'static/images/dummy9.png', 'active'),
-    (10, 'Testing 10', 'This product is just a testing due to the current situation is testing.', 99.99, 10, 10, 'static/images/dummy10.png', 'active');
+    (1, 'Testing 01', 'This product is just a testing due to the current situation is testing.', 9.99, 1, '3DP001A', 'static/images/dummy.png', 'active'),
+    (2, 'Testing 02', 'This product is just a testing due to the current situation is testing.', 19.99, 2, '3DP002V', 'static/images/dummy2.png', 'active'),
+    (3, 'Testing 03', 'This product is just a testing due to the current situation is testing.', 29.99, 3, '3DP003O', 'static/images/dummy3.png', 'active'),
+    (4, 'Testing 04', 'This product is just a testing due to the current situation is testing.', 39.99, 4, 'ELE001K', 'static/images/dummy4.png', 'active'),
+    (5, 'Testing 05', 'This product is just a testing due to the current situation is testing.', 49.99, 5, 'ELE002B', 'static/images/dummy5.png', 'active'),
+    (6, 'Testing 06', 'This product is just a testing due to the current situation is testing.', 59.99, 6, 'ELE003C', 'static/images/dummy6.png', 'active'),
+    (7, 'Testing 07', 'This product is just a testing due to the current situation is testing.', 69.99, 7, 'ELE004W', 'static/images/dummy7.png', 'active'),
+    (8, 'Testing 08', 'This product is just a testing due to the current situation is testing.', 79.99, 8, 'ELE004W', 'static/images/dummy8.png', 'active'),
+    (9, 'Testing 09', 'This product is just a testing due to the current situation is testing.', 89.99, 9, 'ELE003C', 'static/images/dummy9.png', 'active'),
+    (10, 'Testing 10', 'This product is just a testing due to the current situation is testing.', 99.99, 10, 'ELE002B', 'static/images/dummy10.png', 'active'),
+    (10, 'Testing 11', 'This product is just a testing due to the current situation is testing.', 109.99, 11, 'ELE001K', 'static/images/dummy11.png', 'active'),
+    (10, 'Testing 12', 'This product is just a testing due to the current situation is testing.', 119.99, 12, '3DP003O', 'static/images/dummy12.png', 'active'),
+    (10, 'Testing 13', 'This product is just a testing due to the current situation is testing.', 129.99, 13, '3DP002V', 'static/images/dummy13.png', 'active'),
+    (10, 'Testing 14', 'This product is just a testing due to the current situation is testing.', 139.99, 14, '3DP001A', 'static/images/dummy14.png', 'active');
