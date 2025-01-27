c1818a8d61ec17dcf133bccb16543bb0185d9c25
nicktkl <186366129+nicktkl@users.noreply.github.com>
2025-01-27 19:08:36 +0800

    Added Import UUID

--- imports.py
+++ imports.py
@@ -1,13 +1,13 @@
-import os, pytz, random, string, uuid, stripe
-from models import Branch, Category, Product, User, Order, OrderItem, Review, Payment, Feedback, db
 from flask import Flask, Blueprint, render_template, request, redirect, url_for, session, flash, jsonify
-from flask_mail import Mail, Message
-from flask_bcrypt import Bcrypt, bcrypt
-from flask_wtf.csrf import CSRFProtect
+from models import Branch, Category, Product, User, Order, OrderItem, Review, Payment, Feedback, db
+import os, pytz, random, string, uuid, stripe
 from sqlalchemy.sql import func
 from werkzeug.utils import secure_filename
+from flask_mail import Mail, Message
+from flask_bcrypt import Bcrypt, bcrypt
 from itsdangerous import URLSafeTimedSerializer
 from datetime import datetime
 from functools import wraps
+from flask_wtf.csrf import CSRFProtect
 
 csrf = CSRFProtect()
